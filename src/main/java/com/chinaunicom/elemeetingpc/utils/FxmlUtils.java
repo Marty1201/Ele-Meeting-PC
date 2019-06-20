b5417908a31d14b216a7bcc26ea205b0b103c5f2
chenxi <18603854251@wo.cn>
2019-06-20 15:03:01 +0800

    添加语言国际化功能，增加3个新文件（FxmlUtils，messgaes和message_zh），删除之前新建项目默认生成的3个文件（FXMLController.java，Scene.fxml和JavaFXControlsCSS）

---
+++ src/main/java/com/chinaunicom/elemeetingpc/utils/FxmlUtils.java
@@ -0,0 +1,53 @@
+
+package com.chinaunicom.elemeetingpc.utils;
+
+import java.util.ResourceBundle;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+/**
+ * Initialize and load the project's first fxml template, set localization
+ * for the project.
+ * @author chenxi
+ * 创建时间：2019-6-20 9:44:25
+ */
+public class FxmlUtils {
+    
+    /**
+     * Load the project's first fxml template, set localization
+     * 
+     * @param fxmlPath the path of the fxml template
+     * @return Pane
+     */
+    public static Pane fxmlLoader(String fxmlPath) {
+        FXMLLoader loader = new FXMLLoader(FxmlUtils.class.getResource(fxmlPath));
+        loader.setResources(getResourceBundle());
+        try{
+            return loader.load();
+        } catch(Exception e){
+            //to do dialogs
+            //Dialogs.errorAlert(e.getMessage());
+        }
+        return null;
+    }
+    
+    /**
+     * Load the locale language properties file
+     *
+     * @return ResourceBundle
+     */
+    public static ResourceBundle getResourceBundle() {
+        return ResourceBundle.getBundle("bundles.messages");
+    }
+    
+    /**
+     * Return the FXMLLoader
+     *
+     * @return FXMLLoader
+     */
+    public static FXMLLoader getFXMLLoader(String fxmlPath) {
+        FXMLLoader loader = new FXMLLoader(FxmlUtils.class.getResource(fxmlPath));
+        loader.setResources(getResourceBundle());
+        return loader;
+    }
+}
